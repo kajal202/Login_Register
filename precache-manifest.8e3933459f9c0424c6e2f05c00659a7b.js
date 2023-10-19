88f71fe7c6c92ca5f66a56970fcb969b59d8e803
kajal202 <96862427+kajal202@users.noreply.github.com>
2023-10-19 22:26:07 +0530

    Updates

---
+++ precache-manifest.8e3933459f9c0424c6e2f05c00659a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aee798922d775c45f1eaeda7513a5d7",
+    "url": "/Login_Register/index.html"
+  },
+  {
+    "revision": "3deb1df2b0c721aca846",
+    "url": "/Login_Register/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "bfe87205837b1531e7d4",
+    "url": "/Login_Register/static/js/2.1caf4959.chunk.js"
+  },
+  {
+    "revision": "cf1ab768f118db27aab2b789877e4e48",
+    "url": "/Login_Register/static/js/2.1caf4959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3deb1df2b0c721aca846",
+    "url": "/Login_Register/static/js/main.bcd48204.chunk.js"
+  },
+  {
+    "revision": "49fb74e7e22008ccd602",
+    "url": "/Login_Register/static/js/runtime-main.ce7668df.js"
+  }
+]);
